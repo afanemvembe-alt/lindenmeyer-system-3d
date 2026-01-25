26c21d2954dd41aa24b2db3d5e6fa0eae0e60a4b
Adji Marema LO <adji-marema.lo@etu.unicaen.fr>
2026-01-25 23:01:03 +0100

    Modification de SymbolFactory

--- src/lindenmeyer/symbols/SymbolFactory.java
+++ src/lindenmeyer/symbols/SymbolFactory.java
@@ -1,31 +1,23 @@
+package lindenmeyer.symbols;
+
 import java.util.HashMap;
 import java.util.Map;
 
 public class SymbolFactory {
-    // La "Réserve" (ton stock de briques magiques)
+    // Le dictionnaire pour stocker les symboles déjà créés
     private final Map<Character, Symbol> inventory = new HashMap<>();
 
-    // L'alphabet officiel que tu as défini
-    private final String alphabetAutorise = "Ff+-[]XY";
-
     public Symbol getSymbol(char c) {
-        // 1. Vérifier si la lettre fait partie de ton alphabet
-        if (alphabetAutorise.indexOf(c) == -1) {
-            throw new IllegalArgumentException("Erreur : La lettre '" + c + "' ne fait pas partie de l'alphabet autorisé !");
-        }
-
-        // 2. Le Poids-Mouche : on regarde si on a déjà créé cette brique
+        // 1. Si on ne l'a pas encore, on le crée
         if (!inventory.containsKey(c)) {
-            // On ne la crée qu'une seule fois
             inventory.put(c, new Symbol(c));
         }
         
-        // 3. On retourne toujours la même brique stockée
+        // 2. On renvoie celui qui est dans le dictionnaire
         return inventory.get(c);
     }
 
-    // Pour prouver au prof que tu économises de la mémoire
-    public int getNombreDeBriquesEnMemoire() {
+    public int size() {
         return inventory.size();
     }
 }
