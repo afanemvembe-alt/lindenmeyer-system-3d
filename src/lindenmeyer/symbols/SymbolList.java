d7d82a9da376af3b9d1fb2b8e9bcd4d089c1759b
banwell231 <samuel.banwell@etu.unicaen.fr>
2026-01-26 15:09:06 +0100

    ajout de SymbolList

---
+++ src/lindenmeyer/symbols/SymbolList.java
@@ -0,0 +1,35 @@
+package lindenmeyer.symbols;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class SymbolList {
+    private SymbolFactory factory;
+    private ArrayList<Symbol> symbols;
+
+    public SymbolList() {
+        this.factory = new SymbolFactory();
+    }
+
+    public SymbolList(SymbolFactory factory) {
+        this.factory = factory;
+    }
+
+    public void add(Symbol symbol) {
+        this.symbols.add(symbol);
+    }
+
+    public void add(char c) {
+        this.add(this.factory.getSymbol(c));
+    }
+
+    public static SymbolList fromString(String s, SymbolFactory f) {
+        SymbolList res = new SymbolList(f);
+
+        for (int i = 0; i < s.length(); i++) {
+            res.add(s.charAt(i));
+        }
+
+        return res;
+    }
+}
