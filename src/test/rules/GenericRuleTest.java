13b3f89f32e2ad6966aa14283c91e607bab5e93d
Thura Htet <lxgsts@gmail.com>
2026-02-02 15:49:58 +0100

    création du package  pour les classes des tests unitaires

---
+++ src/test/rules/GenericRuleTest.java
@@ -0,0 +1,11 @@
+package test.rules;
+
+import lindenmeyer.rules.GenericRule;
+
+import lindenmeyer.symbols.Symbol;
+import lindenmeyer.symbols.SymbolFactory;
+import lindenmeyer.symbols.SymbolList;
+
+/**
+ * Une classe qui fournit les tests pour une `{@link GenericRule}`
+ */
