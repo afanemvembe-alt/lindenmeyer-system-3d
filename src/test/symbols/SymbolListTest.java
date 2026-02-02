a342b41902261ac5edc724b6692ca2721d3cc937
banwell231 <samuel.banwell@etu.unicaen.fr>
2026-02-02 16:13:04 +0100

    add test for SymbolList

---
+++ src/test/symbols/SymbolListTest.java
@@ -0,0 +1,79 @@
+package test.symbols;
+
+import java.util.List;
+
+import lindenmeyer.symbols.*;
+
+public class SymbolListTest {
+    public static void main(String[] args) {
+        boolean res = true;
+
+        res &= newList();
+        res &= addItem();
+
+        System.out.println(res);
+    }
+
+    public static boolean newList() {
+        boolean res = true;
+
+        SymbolList s = new SymbolList();
+
+        res &= s instanceof SymbolList;
+        res &= s.getSymbols() instanceof List;
+        res &= s.getSymbols().isEmpty();
+
+        return res;
+    }
+
+    public static boolean addItem() {
+        boolean res = true;
+
+        SymbolList s = new SymbolList();
+        
+        Symbol a = new Symbol('A');
+        s.add(a);
+
+        res &= s.getSymbols().contains(a);
+
+        return res;
+    }
+
+    public static boolean addCharacter() {
+        boolean res = true;
+
+        SymbolList s = new SymbolList();
+        SymbolFactory f = new SymbolFactory();
+
+        Symbol a = f.getSymbol('A');
+
+        s.add('A');
+
+        res &= s.getSymbols().contains(a);
+
+        return res;
+    }
+
+    public static boolean listsEqual() {
+        boolean res = true;
+
+        SymbolFactory f = new SymbolFactory();
+
+        SymbolList la = new SymbolList(f);
+        SymbolList lb = new SymbolList(f);
+        SymbolList lc = new SymbolList(f);
+
+        la.add('A');
+        lb.add('A');
+        lc.add('A');
+
+        la.add('B');
+        lb.add('B');
+        lc.add('C');
+
+        res &= la.equals(lb);
+        res &= !la.equals(lc);
+
+        return res;
+    }
+}
