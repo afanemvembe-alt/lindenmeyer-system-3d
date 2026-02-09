3309bc40665aa2fbcc7fbcd3b5d30b6378448723
Nicole Afane <afanemv251@unicaen.fr>
2026-02-09 16:46:45 +0100

    Déplacement de FenetreLSystem dans le package interface et ajout du nouveau fichier GUI

---
+++ src/lindenmeyer/interface/FenetreLSystem.java
@@ -0,0 +1,51 @@
+package lindenmeyer.lsystem;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.*;
+
+public class FenetreLSystem extends JFrame {
+
+    public FenetreLSystem() {
+        super("Petit-Test du LSystem");
+
+        this.setSize(500, 300);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JPanel Monpanel = new JPanel();
+
+        JLabel Axiomee = new JLabel("Axiome :");
+        JTextField ChampDeLaxiome = new JTextField("F+F--F+F", 30);
+
+        JButton BouttonDeGeneration = new JButton("Générer");
+        JButton BouttonPourEffacerLeChampAxiome = new JButton("Supprimer");
+
+        JLabel ProduitAttendu  = new JLabel("ProduitAttendu : ");
+
+        Monpanel.add(Axiomee);
+        Monpanel.add(ChampDeLaxiome);
+        Monpanel.add(BouttonDeGeneration);
+        Monpanel.add(ProduitAttendu );
+
+        this.getContentPane().add(Monpanel);
+
+        BouttonDeGeneration.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                String axiome = ChampDeLaxiome.getText();
+
+                LSystem lsys = new LSystem(axiome);
+                lsys.ajouterRegle('F', "F--F+F");
+
+                String result = lsys.generer(1);
+
+                ProduitAttendu .setText("Résultat : " + result);
+            }
+        });
+
+        this.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new FenetreLSystem();
+    }
+}
