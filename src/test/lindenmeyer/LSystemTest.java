18bc22eec7f4d00f2ff4d6c432d9c565d57d5997
banwell231 <samuel.banwell@etu.unicaen.fr>
2026-02-14 16:18:05 +0100

    ecriture de test pour LSystem

--- src/test/lindenmeyer/LSystemTest.java
+++ src/test/lindenmeyer/LSystemTest.java
@@ -7,19 +7,42 @@ import lindenmeyer.rules.RuleSetFactory;
 import lindenmeyer.symbols.SymbolFactory;
 
 import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.junit.jupiter.api.Test;
 
 class LSystemTest {
     // public static void main(String[] args) {
-    //     boolean res = true;
+    // boolean res = true;
 
-    //     res &= n_generations();
+    // res &= n_generations();
 
-    //     System.out.println(LSystemTest.class + (res ? "Tests OK" : "KO"));
+    // System.out.println(LSystemTest.class + (res ? "Tests OK" : "KO"));
     // }
+
+    // chaine prise de: https://en.wikipedia.org/wiki/L-system#Examples_of_L-systems
+    private static String inputString = "A\n" + //
+            "AB\n" + //
+            "ABA\n" + //
+            "ABAAB\n" + //
+            "ABAABABA\n" + //
+            "ABAABABAABAAB\n" + //
+            "ABAABABAABAABABAABABA\n" + //
+            "ABAABABAABAABABAABABAABAABABAABAAB";
+
+    private static List<String> getExpectedStrings() {
+        return inputString.lines().collect(Collectors.toList());
+    }
 
     @Test
     void n_generations() {
+
+        List<String> expected = getExpectedStrings();
+
+        System.err.println(expected.get(2));
+
         SymbolFactory sf = new SymbolFactory();
         RuleSetFactory rf = new RuleSetFactory(sf);
 
@@ -27,6 +50,9 @@ class LSystemTest {
 
         LSystem ls = new LSystem(new Axiom("A"), rs, sf);
 
-        assertEquals(ls.generer(7), "ABAABABAABAABABAABABAABAABABAABAAB");
+        for (int i = 0; i < expected.size(); i++) {
+            assertEquals(expected.get(i), ls.getCurrentGeneration().toString());
+            ls.step();
+        }
     }
 }
