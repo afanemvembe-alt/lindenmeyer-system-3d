0fb7ec9758f90b3fa7b1e856aa28d5e38413d3d5
banwell231 <samuel.banwell@etu.unicaen.fr>
2026-02-28 20:27:17 +0100

    oops I forgor

--- src/test/rules/SimpleRuleTest.java
+++ src/test/rules/SimpleRuleTest.java
@@ -5,6 +5,9 @@ import lindenmeyer.rules.SimpleRule;
 import lindenmeyer.symbols.SymbolFactory;
 import lindenmeyer.symbols.SymbolList;
 
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
 /**
  * Une classe qui fournit les tests pour une `{@link SimpleRule}`
  */
@@ -18,30 +21,37 @@ public class SimpleRuleTest {
     public final static SymbolList aSuccList = SymbolList.fromString(sampleSucc, sf);
     public final static SymbolList aPredList = SymbolList.fromString(samplePred, sf);
 
-    public static void main(String[] args) {
-        boolean res = true;
+    // public static void main(String[] args) {
+    // boolean res = true;
 
-        res &= SimpleRule_construct_Test();
-        res &= SimpleRule_getPredecessor_Test();
+    // // res &= SimpleRule_construct_Test();
+    // // res &= SimpleRule_getPredecessor_Test();
 
-        System.out.println(res);
-    }
+    // System.out.println(res);
+    // }
 
-    public static boolean SimpleRule_construct_Test() {
-        boolean res = true;
+    @Test
+    void SimpleRule_construct_Test() {
+        // boolean res = true;
         GenericRule simpleRule = new SimpleRule(aPredList.get(0), aSuccList);
 
-        res &= simpleRule instanceof SimpleRule;
-        return res;
+        // res &= simpleRule instanceof SimpleRule;
+
+        assertInstanceOf(SimpleRule.class, simpleRule);
+        // return res;
     }
 
-    public static boolean SimpleRule_getPredecessor_Test() {
-        boolean res = true;
+    @Test
+    void SimpleRule_getPredecessor_Test() {
+        // boolean res = true;
         GenericRule simpleRule = new SimpleRule(aPredList.get(0), aSuccList);
 
-        res &= simpleRule.getPredecessor() instanceof SymbolList;
-        res &= simpleRule.getPredecessor().equals(aPredList);
+        // res &= simpleRule.getPredecessor() instanceof SymbolList;
+        // res &= simpleRule.getPredecessor().equals(aPredList);
 
-        return res;
+        assertInstanceOf(SymbolList.class, simpleRule.getPredecessor());
+        assertEquals(aPredList, simpleRule.getPredecessor());
+
+        // return res;
     }
 }
