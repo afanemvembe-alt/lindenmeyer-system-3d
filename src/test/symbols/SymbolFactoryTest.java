d18f47e6a5f0af01bd83b6341b7ac64ee2080a8d
banwell231 <samuel.banwell@etu.unicaen.fr>
2026-02-28 13:46:11 +0100

    reecriture de tests pour junit

--- src/test/symbols/SymbolFactoryTest.java
+++ src/test/symbols/SymbolFactoryTest.java
@@ -1,35 +1,65 @@
 package test.symbols;
 
-import java.util.*;
-import lindenmeyer.symbols.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class SymbolFactoryTest{
-	
-	public boolean getSymbolTest(){
+import org.junit.jupiter.api.Test;
+
+import lindenmeyer.symbols.Symbol;
+import lindenmeyer.symbols.SymbolFactory;
+
+public class SymbolFactoryTest {
+
+	// public boolean getSymbolTest(){
+	// SymbolFactory factory = new SymbolFactory();
+	// char c1= 'A';
+	// char c2= 'B';
+	// char c3= 'C';
+	// Symbol s1= new Symbol(c1);
+	// Symbol s2= new Symbol(c2);
+	// Symbol s3= new Symbol(c3);
+
+	// boolean ok= factory.getSymbol(c1).equals(s1) &&
+	// factory.getSymbol(c2).equals(s2) && factory.getSymbol(c3).equals(s3);
+	// if (!ok){ System.out.println("Erreur : getSymbol() incorrect pour
+	// SymbolFactory");}
+	// return ok;
+	// }
+
+	@Test
+	void getSymbolTest() {
 		SymbolFactory factory = new SymbolFactory();
-		char c1= 'A';
-		char c2= 'B';
-		char c3= 'C';
-		Symbol s1= new Symbol(c1);
-		Symbol s2= new Symbol(c2);
-		Symbol s3= new Symbol(c3);
-		
-		boolean ok= factory.getSymbol(c1).equals(s1) && factory.getSymbol(c2).equals(s2) && factory.getSymbol(c3).equals(s3);
-		if (!ok){ System.out.println("Erreur : getSymbol() incorrect pour SymbolFactory");}
-		return ok;
+		char c1 = 'A';
+		char c2 = 'B';
+		char c3 = 'C';
+		Symbol s1 = new Symbol(c1);
+		Symbol s2 = new Symbol(c2);
+		Symbol s3 = new Symbol(c3);
+
+		assertEquals(factory.getSymbol(c1), s1);
+		assertEquals(factory.getSymbol(c2), s2);
+		assertEquals(factory.getSymbol(c3), s3);
 	}
-	
-	public boolean sizeTest(){
+
+	// public boolean sizeTest(){
+	// SymbolFactory factory = new SymbolFactory();
+	// factory.getSymbol('A');
+	// factory.getSymbol('B');
+	// factory.getSymbol('C');
+
+	// boolean ok= factory.size()==3;
+	// if (!ok){ System.out.println("Erreur : size() incorrect pour
+	// SymbolFactory");}
+	// return ok;
+	// }
+
+	@Test
+	void sizeTest() {
 		SymbolFactory factory = new SymbolFactory();
-		factory.getSymbol('A'); 
-		factory.getSymbol('B'); 
+		factory.getSymbol('A');
+		factory.getSymbol('B');
 		factory.getSymbol('C');
-		
-		boolean ok= factory.size()==3;
-		if (!ok){ System.out.println("Erreur : size() incorrect pour SymbolFactory");}
-		return ok;
-	}
-	
-}
-	
 
+		assertEquals(factory.size(), 3);
+	}
+
+}
--- src/test/symbols/SymbolTest.java
+++ src/test/symbols/SymbolTest.java
@@ -1,46 +1,88 @@
 package test.symbols;
 
-import lindenmeyer.symbols.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class SymbolTest{
-	
-	public boolean getSymbolTest(){
-		char c1= 'A';
-		char c2= 'B';
-		char c3= 'C';
-		Symbol s1= new Symbol(c1);
-		Symbol s2= new Symbol(c2);
-		Symbol s3= new Symbol(c3);
-		
-		boolean ok= s1.getSymbol()==c1 && s2.getSymbol()==c2 && s3.getSymbol()==c3;
-		if (!ok){ System.out.println("Erreur : getSymbol() incorrect pour Symbol");}
-		return ok;
+import org.junit.jupiter.api.Test;
+
+import lindenmeyer.symbols.Symbol;
+
+public class SymbolTest {
+
+	// public boolean getSymbolTest(){
+	// char c1= 'A';
+	// char c2= 'B';
+	// char c3= 'C';
+	// Symbol s1= new Symbol(c1);
+	// Symbol s2= new Symbol(c2);
+	// Symbol s3= new Symbol(c3);
+
+	// boolean ok= s1.getSymbol()==c1 && s2.getSymbol()==c2 && s3.getSymbol()==c3;
+	// if (!ok){ System.out.println("Erreur : getSymbol() incorrect pour Symbol");}
+	// return ok;
+	// }
+
+	@Test
+	void getSymbolTest() {
+		char c1 = 'A';
+		char c2 = 'B';
+		char c3 = 'C';
+		Symbol s1 = new Symbol(c1);
+		Symbol s2 = new Symbol(c2);
+		Symbol s3 = new Symbol(c3);
+
+		assertEquals(s1.getSymbol(), c1);
+		assertEquals(s2.getSymbol(), c2);
+		assertEquals(s3.getSymbol(), c3);
 	}
-	
-	public boolean setSymbolTest(){
-		Symbol s1= new Symbol('A');
-		Symbol s2= new Symbol('B');
-		Symbol s3= new Symbol('C');
-		char c1= 'A';
-		char c2= 'B';
-		char c3= 'C';
+
+	// public boolean setSymbolTest(){
+	// Symbol s1= new Symbol('A');
+	// Symbol s2= new Symbol('B');
+	// Symbol s3= new Symbol('C');
+	// char c1= 'A';
+	// char c2= 'B';
+	// char c3= 'C';
+	// s1.setSymbol(c3);
+	// s2.setSymbol(c1);
+	// s3.setSymbol(c2);
+
+	// boolean ok= s1.getSymbol()==c3 && s2.getSymbol()==c1 && s3.getSymbol()==c2;
+	// if (!ok){ System.out.println("Erreur : setSymbol() incorrect pour Symbol");}
+	// return ok;
+	// }
+
+	@Test
+	void setSymbolTest() {
+		Symbol s1 = new Symbol('A');
+		Symbol s2 = new Symbol('B');
+		Symbol s3 = new Symbol('C');
+		char c1 = 'A';
+		char c2 = 'B';
+		char c3 = 'C';
 		s1.setSymbol(c3);
 		s2.setSymbol(c1);
 		s3.setSymbol(c2);
-		
-		boolean ok= s1.getSymbol()==c3 && s2.getSymbol()==c1 && s3.getSymbol()==c2;
-		if (!ok){ System.out.println("Erreur : setSymbol() incorrect pour Symbol");}
-		return ok;
+
+		assertEquals(s1.getSymbol(), c3);
+		assertEquals(s2.getSymbol(), c1);
+		assertEquals(s3.getSymbol(), c2);
 	}
-	
-	public boolean equalsTest(){
+
+	// public boolean equalsTest(){
+	// Symbol a = new Symbol('A');
+	// Symbol b = new Symbol('A');
+
+	// boolean ok= a.equals(b);
+	// if (!ok){ System.out.println("Erreur : equals() incorrect pour Symbol");}
+	// return ok;
+	// }
+
+	@Test
+	void equalsTest() {
 		Symbol a = new Symbol('A');
 		Symbol b = new Symbol('A');
-		
-		boolean ok= a.equals(b);
-		if (!ok){ System.out.println("Erreur : equals() incorrect pour Symbol");}
-		return ok;
+
+		assertEquals(a, b);
 	}
-	
+
 }
-	
