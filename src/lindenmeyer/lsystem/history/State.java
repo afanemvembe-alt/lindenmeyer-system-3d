150ce3f5afd66a1d31ca67e7f708532c4f79f65c
banwell231 <samuel.banwell@etu.unicaen.fr>
2026-03-09 16:12:20 +0100

    add documentation

--- src/lindenmeyer/lsystem/history/State.java
+++ src/lindenmeyer/lsystem/history/State.java
@@ -2,13 +2,26 @@ package lindenmeyer.lsystem.history;
 
 import lindenmeyer.symbols.SymbolList;
 
+/**
+ * Un état individuel à stocker.
+ */
 public class State {
     private SymbolList state;
 
+    /**
+     * Crée un nouvel état à partir de l'état donné.
+     * 
+     * @param toSaveState l'état à sauvegarder
+     */
     public State(SymbolList toSaveState) {
         state = toSaveState;
     }
 
+    /**
+     * Retourne l'état stocké.
+     * 
+     * @return l'état stocké
+     */
     public SymbolList getState() {
         return state;
     }
