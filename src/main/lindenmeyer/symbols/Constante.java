217b2533d2a05ccabca92006f4d3a6432f42d09e
Adji Marema LO <adji-marema.lo@etu.unicaen.fr>
2026-04-14 14:45:48 +0200

    modidification de la classe constante

--- src/main/lindenmeyer/symbols/Constante.java
+++ src/main/lindenmeyer/symbols/Constante.java
@@ -24,24 +24,28 @@ public class Constante extends Symbol {
     @Override
     public void interpret(AbstractTurtle3D turtle) {
         switch (getSymbol()) {
-            case '[':
-                turtle.savePosition();
-                break;
-            case ']':
-                turtle.restorePosition();
-                break;
-            case '+':
-                turtle.changeAngleX(true);
-                break;
-            case '-':
-                turtle.changeAngleX(false);
-                break;
-            case '*':
-                turtle.changeAngleZ(true);
-                break;
-            case '/':
-                turtle.changeAngleZ(false);
-                break;
+            // --- pile ---
+            case '[': turtle.savePosition();    break;
+            case ']': turtle.restorePosition(); break;
+
+            // --- yaw (RotateU) : tourne dans le plan horizontal ---
+            case '+': turtle.rotateU(true);     break;
+            case '-': turtle.rotateU(false);    break;
+
+            // --- pitch (RotateL) : tourne dans le plan vertical ---
+            case '&': turtle.rotateL(true);     break;
+            case '^': turtle.rotateL(false);    break;
+
+            // --- roll (RotateH) : roule autour de l'axe de marche ---
+            case '\\': turtle.rotateH(true);    break;
+            case '/':  turtle.rotateH(false);   break;
+
+            // --  utiliser comme rotation Z -> on le mappe sur pitch ---
+            case '*': turtle.changeAngleZ(true); break;
+
+            // --- demi-tour : inverse heading et left ---
+            case '|': turtle.rotateU(true);
+                      turtle.rotateU(true); break; // deux fois l'angle si angle=90°
         }
     }
 }
